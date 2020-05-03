88bd03913228ac6180d87585b8d26b400027d93c
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-05-03 22:52:48 +0200

    added context for currentUser and collections and added a provider for the cart covering cartItems, adding items to cart, and toggling the cart dropdown

---
+++ src/providers/cart/cart.provider.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+
+export const CartContext = createContext({
+    hidden: true,
+    toggleHidden: () => {},
+    cartItems: [],
+    addItem: () => {},
+    removeItem: () => {},
+    clearItemFromCart: () => {},
+    cartItemsCount: 0,
+});
+
+const CartProvider = ({ children }) => {
+    const [hidden, setHidden] = useState(true);
+    const [cartItems, setCartItems] = useState([]);
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+    
+    const toggleHidden = () => setHidden(!hidden);
+    const addItem = item => setCartItems(addItemToCart(cartItems, item));
+
+    return(
+        <CartContext.Provider
+           value={{
+               hidden,
+               toggleHidden,
+               cartItems,
+               addItem,
+               cartItemsCount,
+           }} 
+        >
+            { children }
+        </CartContext.Provider>
+
+    )
+};
+
+export default CartProvider;
